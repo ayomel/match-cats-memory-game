1478d4398426f9fee08810e648d73281a4f73633
Mel <huntermelverton@gmail.com>
2022-06-02 18:08:01 -0400

    fixup: fix card image src

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import SingleCard from './SingleCard';
-import './Home.css';
+import React, { useState, useEffect } from 'react'
+import SingleCard from './SingleCard'
+import './Home.css'
 // Images
-import cover from "../cat-photos/match_cat_cover.png";
-import abyssinian from "../cat-photos/abyssinian.png";
-import birman from "../cat-photos/birman.jpg"
-import chantilly from "../cat-photos/chantilly.jpg"
-import chartreux from "../cat-photos/chartreux.jpg"
-import devonrex from "../cat-photos/devonrex.png"
-import mainecoon from "../cat-photos/mainecoon.jpg"
-import manx from "../cat-photos/manx.jpg"
-import ragamuffin from "../cat-photos/ragamuffin.jpg"
-import ragdoll from "../cat-photos/ragdoll.jpg"
-import russianblue from "../cat-photos/russianblue.jpg"
+import cover from '../cat-photos/match_cat_cover.png'
+import abyssinian from '../cat-photos/abyssinian.png'
+import birman from '../cat-photos/birman.jpg'
+import chantilly from '../cat-photos/chantilly.jpg'
+import chartreux from '../cat-photos/chartreux.jpg'
+import devonrex from '../cat-photos/devonrex.png'
+import mainecoon from '../cat-photos/mainecoon.jpg'
+import manx from '../cat-photos/manx.jpg'
+import ragamuffin from '../cat-photos/ragamuffin.jpg'
+import ragdoll from '../cat-photos/ragdoll.jpg'
+import russianblue from '../cat-photos/russianblue.jpg'
 
 const cardImages = [
   { src: abyssinian, matched: false },
@@ -24,86 +24,107 @@ const cardImages = [
   { src: manx, matched: false },
   { src: ragamuffin, matched: false },
   { src: ragdoll, matched: false },
-  { src: russianblue, matched: false }
-];
+  { src: russianblue, matched: false },
+]
 
 function Home() {
-  const [cards, setCards] = useState([]);
-  const [turns, setTurns] = useState(0);
-  const [choiceOne, setChoiceOne] = useState(null);
-  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [ cards, setCards ] = useState([])
+  const [ turns, setTurns ] = useState(0)
+  const [ choiceOne, setChoiceOne ] = useState(null)
+  const [ choiceTwo, setChoiceTwo ] = useState(null)
 
   // Shuffle cards
   const shuffleCards = () => {
-    const shuffled = [...cardImages, ...cardImages]
-      shuffled.sort(() => Math.random() - 0.5)
+    const shuffled = [ ...cardImages, ...cardImages ]
+    shuffled
+      .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))
-    
+
     setCards(shuffled)
     setTurns(0)
   }
   // Set new game on home load
   useEffect(() => {
-    shuffleCards();
-  }, []);
+    shuffleCards()
+  }, [])
 
   // Handle a choice
   const handleChoice = (card) => {
-    // If choiceOne is not null, setChoiceTwo. If it is null, setChoiceOne
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+    if (choiceOne === null && choiceTwo === null) {
+      setChoiceOne(card)
+    } else {
+      setChoiceTwo(card)
+    }
   }
 
-  // Compare two selected cards
-  useEffect(() => {
-    if(choiceOne && choiceTwo) {
-
-      if(choiceOne.src === choiceTwo.src) {
-        console.log("matched.");
-        setCards(prevCards => {
-          return prevCards.map(card => {
-            if (card.src === choiceOne.src) {
-              return {...card, matched: true}
+  useEffect(
+    () => {
+      if (choiceOne === choiceTwo && (choiceOne && choiceTwo)) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne) {
+              return { ...card, matched: true }
             } else {
-              return card;
+              return card
             }
           })
         })
-        resetTurn();
-      } else {
-        
-        resetTurn();
+        resetTurn()
+      } else if (choiceOne && choiceTwo) {
+        resetTurn()
       }
-    }
-  }, [choiceOne, choiceTwo])
-  console.log(cards);
+    },
+    [ choiceOne, choiceTwo ],
+  )
+
+  // Compare two selected cards
+  // useEffect(
+  //   () => {
+  //     if (choiceOne === choiceTwo && (choiceOne && choiceTwo)) {
+  //       console.log('matched.')
+  //       setCards((prevCards) => {
+  //         return prevCards.map((card) => {
+  //           if (card.src === choiceOne.src) {
+  //             return { ...card, matched: true }
+  //           } else {
+  //             return card
+  //           }
+  //         })
+  //       })
+  //       resetTurn()
+  //     } else {
+  //       resetTurn()
+  //     }
+  //   },
+  //   [ choiceOne, choiceTwo ],
+  // )
 
   // Reset choices & increase turn
   const resetTurn = () => {
-    setChoiceOne(null);
-    setChoiceTwo(null);
-    setTurns(prevTurns => prevTurns + 1);
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns((prevTurns) => prevTurns + 1)
   }
 
-
   return (
-    <>
+    <React.Fragment>
       <h1>Match Cats!</h1>
       <button onClick={shuffleCards}>New Game</button>
-    
-      <div className="card-grid">
-        {cards.map(card => {
+
+      <div className='card-grid'>
+        {cards.map((card, i) => {
           return (
             <SingleCard
-              key={card.id} 
-              card={card.src} 
+              key={i}
+              card={card.src}
               cover={cover}
-              handleChoice={handleChoice} 
+              handleChoice={handleChoice}
             />
-          );
+          )
         })}
       </div>
-    </>
-  );
+    </React.Fragment>
+  )
 }
 
-export default Home;
+export default Home
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,24 +1,24 @@
-import React from "react";
-import './SingleCard.css';
+import React from 'react'
+import './SingleCard.css'
 
-function SingleCard({ key, card, cover, handleChoice }) {
+function SingleCard({ card, cover, handleChoice }) {
   const handleClick = () => {
-    handleChoice(card);
+    handleChoice(card)
   }
 
-    return (
-        <div className="card" key={key}>
-             <div>
-               <img className="front" src={card} alt="card front" />
-               <img 
-                className="back" 
-                src={cover} 
-                alt="card back" 
-                onClick={handleClick} 
-                />
-             </div>
-            </div>
-    );
+  return (
+    <div className='card'>
+      <div>
+        <img className='front' src={card} alt='card front' />
+        <img
+          className='back'
+          src={cover}
+          alt='card back'
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  )
 }
 
-export default SingleCard;
+export default SingleCard
